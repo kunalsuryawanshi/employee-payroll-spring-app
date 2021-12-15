774c532cc7ad2ceaacf30052bddd34d2acc52fe1
kunal <43981189+kunalsuryawanshi@users.noreply.github.com>
2021-12-16 01:44:14 +0530

    Added: Negative Test Cases for get, update and delete Methods

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDto.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+
 /**
  * Purpose: To Get Data From End User
  *
@@ -13,16 +14,22 @@ import javax.validation.constraints.Pattern;
 @Data
 public class EmployeeDto {
     @NotNull
-    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}([\\s][A-Z]{1}[a-z]{2,})?$", message = "at least 3 Characters and First Letter Should be Capital")
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}([\\s][A-Z]{1}[a-z]{2,})?$", message = "Minimum 3 Characters and First Letter Should be Capital")
     private String name;
 
+    @NotNull
     private String imagePath;
 
+    @NotNull
+    @Pattern(regexp = "(m|M|male|Male|f|F|female|Female)", message = "Gender Should be Male or Female")
     private String gender;
 
+    @NotNull
     private String salary;
 
+    @NotNull
     private String department;
 
+    @NotNull
     private String notes;
 }
